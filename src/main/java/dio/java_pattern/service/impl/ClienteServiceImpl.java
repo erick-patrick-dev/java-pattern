e5a8b792c65d366bacf9930b75e0883dfa8f28e2
Homura-san <erickbispo@ufba.br>
2026-02-27 21:24:54 -0300

    Implementação da regra de negócio

--- src/main/java/dio/java_pattern/service/impl/ClienteServiceImpl.java
+++ src/main/java/dio/java_pattern/service/impl/ClienteServiceImpl.java
@@ -1,33 +1,65 @@
 package dio.java_pattern.service.impl;
 
 import dio.java_pattern.model.Cliente;
+import dio.java_pattern.model.ClienteRepository;
+import dio.java_pattern.model.Endereco;
+import dio.java_pattern.model.EnderecoRepository;
 import dio.java_pattern.service.ClienteService;
+import dio.java_pattern.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        var res = clienteRepository.findById(id);
+        return res.orElse(null);
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        salvarClienteComCep(cliente);
     }
+
+
 
     @Override
     public void atualizar(Long id, Cliente cliente) {
+        var clienteBd = clienteRepository.findById(id);
 
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
     }
 
     @Override
     public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
 
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 }
